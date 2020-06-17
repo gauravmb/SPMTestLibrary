c3f9a8de443a1c61596c5c364ce5b43f618e91f1
gaurav.bhatia <gaurav.bhatia@hurix.com>
2020-06-17 16:27:45 +0530

    First Commit

---
+++ Sources/MyLibrary/MyClass.swift
@@ -0,0 +1,19 @@
+//
+//  MyClass.swift
+//  TestSwift
+//
+//  Created by Gaurav Bhatia on 17/06/20.
+//  Copyright © 2020 Gaurav Bhatia. All rights reserved.
+//
+
+import Foundation
+
+public struct MyLibraryClass {
+    
+    
+    public static func methodInsideMyLibraryClass()
+    {
+        print("Inside methodInsideMyLibraryClass")
+    }
+    
+}
